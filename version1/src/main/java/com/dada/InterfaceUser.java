37c2f3d3b48eede03ab33a86ab2758ce0eef8240
Ariel Mendoza <medifbryan@gmail.com>
2025-06-16 23:13:12 -0500

    new code

---
+++ version1/src/main/java/com/dada/InterfaceUser.java
@@ -0,0 +1,52 @@
+package com.dada;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+
+
+
+public class InterfaceUser {
+
+private JPanel jPanel;
+private JLabel label;
+
+
+
+      public static void UserSEE(){
+
+
+         JButton[] buttones  = new JButton[4];  
+
+
+
+         buttones[0] = new JButton("SISTEMA DE GESTION FINANZAS Y INVERSIONES");
+
+
+         buttones[1] = new JButton("agregar cliente nuevo");
+          
+
+         buttones[2] = new JButton("eliminar cliente");
+
+         buttones[3] = new JButton("Servicios & productos");
+
+         buttones[4] = new JButton("fecha pago");
+          
+         buttones[5] = new JButton("salir");
+
+
+         
+
+
+
+
+
+
+      }  
+
+      
+
+    
+}
--- version1/src/main/java/com/dada/Main.java
+++ version1/src/main/java/com/dada/Main.java
@@ -3,9 +3,7 @@ package com.dada;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        InterfaceUser oInterfaceUser = new InterfaceUser();
 
-        oInterfaceUser.InterfaceU();
-    }
 
+}  
 }
