7d085e2e354b3af0c348e9c37c39ed4e99958c13
Ariel Mendoza <medifbryan@gmail.com>
2025-06-16 20:53:13 -0500

    migracion de versiones excelente parte visual lista

---
+++ version1/src/main/java/com/dada/Main.java
@@ -0,0 +1,11 @@
+package com.dada;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        InterfaceUser oInterfaceUser = new InterfaceUser();
+
+        oInterfaceUser.InterfaceU();
+    }
+
+}
